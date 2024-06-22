226bb1afefa480991be1d4b9cc325a558293b0b3
Amir Syed <75707901+amirrsyed@users.noreply.github.com>
2024-06-22 15:06:10 -0400

    Create blockchain.py

---
+++ blockchain.py
@@ -0,0 +1,47 @@
+import time
+import hashlib
+import json
+
+class Block:
+    def __init__(self, index, previous_hash, timestamp, data, nonce=0):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.timestamp = timestamp
+        self.data = data
+        self.nonce = nonce
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        block_string = f"{self.index}{self.previous_hash}{self.timestamp}{self.data}{self.nonce}"
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
+    def mine_block(self, difficulty):
+        while self.hash[:difficulty] != '0' * difficulty:
+            self.nonce += 1
+            self.hash = self.calculate_hash()
+
+class Blockchain:
+    def __init__(self, difficulty=4):
+        self.chain = [self.create_genesis_block()]
+        self.difficulty = difficulty
+
+    def create_genesis_block(self):
+        return Block(0, "0", int(time.time()), "Genesis Block")
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, new_block):
+        new_block.previous_hash = self.get_latest_block().hash
+        new_block.mine_block(self.difficulty)
+        self.chain.append(new_block)
+
+    def is_chain_valid(self):
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i-1]
+            if current_block.hash != current_block.calculate_hash():
+                return False
+            if current_block.previous_hash != previous_block.hash:
+                return False
+        return True
